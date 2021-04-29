63dfaa8cbe92a15c78e2b8202ea21d0e790dc8b1
Zits <79975901+Zitss@users.noreply.github.com>
2021-04-30 00:10:15 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -15,11 +15,11 @@ router.get('/config', (req, res) => {
     config = {
         status: true,
         result: {
-            prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            prefix : 'MultiPrefix',
+            namabot: 'ZitsraaBOR',
+            namaowner: 'Fmrzyy',
+            instagram: 'zitsraa_',
+            youtube : 'Zitsraaxyz'
         }
     }
     res.json(config)
